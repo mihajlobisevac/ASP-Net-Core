bd4741f84ccbcb514cca2576276777abb04364b1
Mihajlo Biševac <kajler11@gmail.com>
2020-08-27 07:00:18 +0200

    Added a way to edit users

--- EmployeeManagment/Controllers/AdministrationController.cs
+++ EmployeeManagment/Controllers/AdministrationController.cs
@@ -32,6 +32,67 @@ namespace EmployeeManagment.Controllers
         }
 
         [HttpGet]
+        public async Task<IActionResult> EditUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with id {id} not found";
+                return View("NotFound");
+            }
+
+            var userRoles = await userManager.GetRolesAsync(user);
+            var userClaims = await userManager.GetClaimsAsync(user);
+
+            var model = new EditUserViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                UserName = user.UserName,
+                City = user.City,
+                Roles = userRoles,
+                Claims = userClaims.Select(c => c.Value).ToList()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUser(EditUserViewModel model)
+        {
+            var user = await userManager.FindByIdAsync(model.Id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with id {model.Id} not found";
+                return View("NotFound");
+            }
+            else
+            {
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+                user.City = model.City;
+
+                var result = await userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListUsers");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+
+                return View(model);
+            }
+        }
+
+        [HttpGet]
         public IActionResult CreateRole()
         {
             return View();
---
+++ EmployeeManagment/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagment.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string City { get; set; }
+        public IList<string> Roles { get; set; } = new List<string>();
+        public IList<string> Claims { get; set; } = new List<string>();
+    }
+}
