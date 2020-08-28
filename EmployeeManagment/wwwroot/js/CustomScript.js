c57d5e9c704108dfd84ba09497ae89f807706ff0
Mihajlo Biševac <kajler11@gmail.com>
2020-08-28 19:12:50 +0200

    Added delete confirmation for users

--- EmployeeManagment/wwwroot/js/CustomScript.js
+++ EmployeeManagment/wwwroot/js/CustomScript.js
@@ -1,1 +1,12 @@
-﻿
+﻿function confirmDelete(id, isTrue){
+    var deleteSpan = document.getElementById(`deleteSpan_${id}`)
+    var confirmDeleteSpan = document.getElementById(`confirmDeleteSpan_${id}`)
+
+    if (isTrue) {
+        deleteSpan.style.display = "none"
+        confirmDeleteSpan.style.display = "inline"
+    } else {
+        deleteSpan.style.display = "inline"
+        confirmDeleteSpan.style.display = "none"
+    }
+}
